821290a7febc576b1a4159500bedab85a7b695aa
benjaminch <benjamin.chastanier@gmail.com>
2026-01-25 16:08:11 +0100

    chore: bump version to 0.1.2

--- Formula/sobriquet.rb
+++ Formula/sobriquet.rb
@@ -1,12 +1,12 @@
 class Sobriquet < Formula
   desc "Fuzzy finder for shell aliases"
   homepage "https://github.com/benjaminch/sobriquet"
-  url "https://github.com/benjaminch/sobriquet/archive/refs/tags/v0.1.1.tar.gz"
+  url "https://github.com/benjaminch/sobriquet/archive/refs/tags/v0.1.2.tar.gz"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/benjaminch/sobriquet/releases/download/v0.1.1"
+    root_url "https://github.com/benjaminch/sobriquet/releases/download/v0.1.2"
     sha256 cellar: :any_skip_relocation, arm64_sequoia:  "0000000000000000000000000000000000000000000000000000000000000000"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0000000000000000000000000000000000000000000000000000000000000000"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "0000000000000000000000000000000000000000000000000000000000000000"
