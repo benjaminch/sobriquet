c7557407429070b4fef1f5cef8cb261839315661
benjaminch <benjamin.chastanier@gmail.com>
2026-01-25 18:14:00 +0100

    fix: use CARGO_PKG_VERSION in version test instead of hardcoded version

--- tests/cli/version.rs
+++ tests/cli/version.rs
@@ -5,12 +5,14 @@ use predicates::prelude::*;
 
 #[test]
 fn shows_version_with_flag() {
+    // Get the version from Cargo.toml at runtime
+    let version = env!("CARGO_PKG_VERSION");
     alx()
         .arg("--version")
         .assert()
         .success()
         .stdout(predicate::str::contains("sobriquet"))
-        .stdout(predicate::str::contains("0.1.0"));
+        .stdout(predicate::str::contains(version));
 }
 
 #[test]
